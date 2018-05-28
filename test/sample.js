ac6f8ace68b35089e6f7510a7f7d4b344d6bab8d
alexsegal <asegal2012@gmail.com>
2018-05-28 13:22:59 -0400

    just update to push new

--- test/sample.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-
-describe("Something", function () {
-    describe("The way it smells", function () {
-        it("is pungent", function () {
-            assert.ok(true);
-        });
-    });
-}) ;
